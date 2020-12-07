78f9d5040c2e0a32a28f085542188639aea849f8
ptmx <phil.tm@gmail.com>
2020-12-07 17:07:40 -0500

    Puzzle 7a solution in PostgreSQL

---
+++ 07/07a.sql
@@ -0,0 +1,58 @@
+DROP TABLE IF EXISTS raw_input;
+DROP TABLE IF EXISTS bag_pairs;
+
+CREATE TABLE raw_input (
+  raw_input_line TEXT NOT NULL
+);
+
+\COPY raw_input FROM 'input.txt';
+
+CREATE TABLE bag_pairs AS
+WITH
+  split_containing_bag AS (
+    SELECT 
+      REGEXP_SPLIT_TO_ARRAY(raw_input_line, ' bags contain ') AS split_line
+    FROM
+      raw_input
+  ),
+  split_contained_bags AS (
+    SELECT
+      split_line[1] AS containing_bag_colour,
+      REGEXP_SPLIT_TO_TABLE(TRIM(split_line[2], '.'), ', ') AS contained_bags
+    FROM
+      split_containing_bag
+  ),
+  containing_bag_pairs AS (
+    SELECT
+      containing_bag_colour,
+      REGEXP_MATCH(contained_bags, '^(\d+) (.+) bags?$') AS contained_bags_match
+    FROM
+      split_contained_bags
+  )
+SELECT
+  containing_bag_colour,
+  contained_bags_match[2] AS contained_bag_colour,
+  contained_bags_match[1]::INT AS contained_bag_count 
+FROM
+  containing_bag_pairs;
+
+WITH RECURSIVE possible_outer_bags AS (
+  SELECT
+    containing_bag_colour AS outer_bag,
+    contained_bag_colour AS inner_bag
+  FROM
+    bag_pairs
+  WHERE
+    contained_bag_colour = 'shiny gold'
+  UNION
+  SELECT
+    containing_bag_colour,
+    contained_bag_colour
+  FROM 
+    bag_pairs
+    JOIN possible_outer_bags ON (
+      bag_pairs.contained_bag_colour = possible_outer_bags.outer_bag
+    )
+)
+SELECT COUNT(DISTINCT outer_bag)
+FROM possible_outer_bags;
